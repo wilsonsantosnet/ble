881656d302b42efeaeb5dddf49fdea8032279ff1
Wilson dos Santos <wilsonsantosnet@gmail.com>
2021-09-23 14:44:30 -0300

    initial

---
+++ Blueberry.Desktop.ConsolePlayground/Program.cs
@@ -0,0 +1,122 @@
+﻿using Blueberry.Desktop.WindowsApp.Bluetooth;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blueberry.Desktop.ConsolePlayground
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            var tcs = new TaskCompletionSource<bool>();
+
+            Task.Run(async () =>
+            {
+                try
+                {
+                    // New watcher
+                    var watcher = new DnaBluetoothLEAdvertisementWatcher(new GattServiceIds());
+
+                    // Hook into events
+                    watcher.StartedListening += () =>
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("Started listening");
+                    };
+
+                    watcher.StoppedListening += () =>
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine("Stopped listening");
+                    };
+
+                    watcher.NewDeviceDiscovered += (device) =>
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"New device: {device}");
+                    };
+
+                    watcher.DeviceNameChanged += (device) =>
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"Device name changed: {device}");
+                    };
+
+                    watcher.DeviceTimeout += (device) =>
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Device timeout: {device}");
+                    };
+
+                    // Start listening
+                    watcher.StartListening();
+
+                    while (true)
+                    {
+                        // Pause until we press enter
+                        var command = Console.ReadLine()?.ToLower().Trim();
+
+                        if (string.IsNullOrEmpty(command))
+                        {
+                            // Get discovered devices
+                            var devices = watcher.DiscoveredDevices;
+
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine($"{devices.Count} devices......");
+
+                            foreach (var device in devices)
+                                Console.WriteLine(device);
+                        }
+                        else if (command == "c")
+                        {
+                            // Attempt to find contour device
+                            var contourDevice = watcher.DiscoveredDevices.FirstOrDefault(
+                                f => f.Name.ToLower().Contains("contour"));
+
+                            // If we don't find it...
+                            if (contourDevice == null)
+                            {
+                                // Let the user know
+                                Console.WriteLine("No Contour device found for connecting");
+                                continue;
+                            }
+
+                            // Try and connect
+                            Console.WriteLine("Connecting to Contour Device...");
+
+                            try
+                            {
+                                // Try and connect
+                                await watcher.PairToDeviceAsync(contourDevice.DeviceId);
+                            }
+                            catch (Exception ex)
+                            {
+                                // Log it out
+                                Console.WriteLine("Failed to pair to Contour device.");
+                                Console.WriteLine(ex);
+                            }
+                        }
+                        // Q to quit
+                        else if (command == "q")
+                        {
+                            break;
+                        }
+                    }
+
+                    // Finish console application
+                    tcs.TrySetResult(true);
+                }
+                finally
+                {
+                    // If anything goes wrong, exit out
+                    tcs.TrySetResult(false);
+                }
+            });
+
+            tcs.Task.Wait();
+        }
+    }
+}
---
+++ server.ble/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+
+namespace server.ble
+{
+    class Program
+    {
+
+        static void Main(string[] args) {
+
+            ExecuteAsync(args).Wait();
+            Console.Read();
+        }
+        static async Task ExecuteAsync(string[] args)
+        {
+
+            var uuid = Guid.NewGuid();
+            var uuid1 = Guid.NewGuid();
+            var uuid2 = Guid.NewGuid();
+            var uuid3 = Guid.NewGuid();
+
+            GattServiceProvider serviceProvider = null;
+            var result = await GattServiceProvider.CreateAsync(uuid);
+
+            if (result.Error == BluetoothError.Success)
+            {
+                serviceProvider = result.ServiceProvider;
+            }
+
+            var value = new byte[] { 0x21 };
+            var constantParametersRead = new GattLocalCharacteristicParameters
+            {
+                CharacteristicProperties = (GattCharacteristicProperties.Read),
+                StaticValue = value.AsBuffer(),
+                ReadProtectionLevel = GattProtectionLevel.Plain,
+                UserDescription = "teste1"
+            };
+
+            var characteristicResult = await serviceProvider.Service.CreateCharacteristicAsync(uuid1, constantParametersRead);
+            if (characteristicResult.Error != BluetoothError.Success)
+            {
+                // An error occurred.
+                return;
+            }
+
+            var constantParametersWrite = new GattLocalCharacteristicParameters
+            {
+                CharacteristicProperties = (GattCharacteristicProperties.Write),
+                StaticValue = value.AsBuffer(),
+                ReadProtectionLevel = GattProtectionLevel.Plain,
+                UserDescription = "teste2"
+            };
+            var _readCharacteristic = characteristicResult.Characteristic;
+            _readCharacteristic.ReadRequested += ReadCharacteristic_ReadRequested;
+
+            characteristicResult = await serviceProvider.Service.CreateCharacteristicAsync(uuid2, constantParametersWrite);
+            if (characteristicResult.Error != BluetoothError.Success)
+            {
+                // An error occurred.
+                return;
+            }
+
+            var constantParametersNotify = new GattLocalCharacteristicParameters
+            {
+                CharacteristicProperties = (GattCharacteristicProperties.Notify),
+                StaticValue = value.AsBuffer(),
+                ReadProtectionLevel = GattProtectionLevel.Plain,
+                UserDescription = "teste3"
+            };
+            var _writeCharacteristic = characteristicResult.Characteristic;
+            _writeCharacteristic.WriteRequested += WriteCharacteristic_WriteRequested;
+
+            characteristicResult = await serviceProvider.Service.CreateCharacteristicAsync(uuid3, constantParametersNotify);
+            if (characteristicResult.Error != BluetoothError.Success)
+            {
+                // An error occurred.
+                return;
+            }
+            var _notifyCharacteristic = characteristicResult.Characteristic;
+            _notifyCharacteristic.SubscribedClientsChanged += SubscribedClientsChanged;
+
+
+            var advParameters = new GattServiceProviderAdvertisingParameters
+            {
+                IsDiscoverable = true,
+                IsConnectable = true
+            };
+            serviceProvider.StartAdvertising(advParameters);
+
+            Console.WriteLine($"{uuid}|{uuid1}|{uuid2}|{uuid3}");
+        }
+
+        private static void SubscribedClientsChanged(GattLocalCharacteristic sender, object args)
+        {
+            Console.WriteLine("SubscribedClientsChanged!");
+        }
+
+        private static void WriteCharacteristic_WriteRequested(GattLocalCharacteristic sender, GattWriteRequestedEventArgs args)
+        {
+            Console.WriteLine("WriteCharacteristic_WriteRequested!");
+        }
+
+        private static void ReadCharacteristic_ReadRequested(GattLocalCharacteristic sender, GattReadRequestedEventArgs args)
+        {
+            Console.WriteLine("ReadCharacteristic_ReadRequested!");
+        }
+    }
+}
